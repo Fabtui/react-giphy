717dedd27a23c6a61443fe5a196cb2c113244e8f
Fabrice Tuilliere <fabrice.tui@gmail.com>
2021-09-18 13:50:53 +0200

    add effects on over

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Gif extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mouseOver: false
+    }
+  }
+
   handleClick = () => {
     if (this.props.selectGif) {
       this.props.selectGif(this.props.id)
     }
   }
 
+  onMouseEnter = () => {
+    this.setState({ mouseOver: true })
+  }
+
+  onMouseOut = () => {
+    this.setState({ mouseOver: false })
+  }
+
+  classeNames = () => {
+    let classes = 'gif'
+    if (this.state.mouseOver) {
+      classes += ' over'
+    } else classes += ' blur'
+    return classes
+  }
+
   render() {
     const src = `https://media2.giphy.com/media/${this.props.id}/200w.gif`
     return (
-      <img src={src} alt="" className="gif" onClick={this.handleClick} />
+      <img src={src} alt="" className={this.classeNames()} onClick={this.handleClick} onMouseOut={this.onMouseOut} onMouseEnter={this.onMouseEnter} />
     );
   }
 }
